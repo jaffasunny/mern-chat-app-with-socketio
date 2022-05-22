0e74a8970d4d0e93b3fc0b4491e06e0161667d41
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-05-22 12:37:24 -0700

    signup and login page completed

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -5,20 +5,79 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     setShow(!show);
   };
 
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please fill all the fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing={"5px"} align='stretch' color={"black"}>
@@ -26,6 +85,7 @@ const Login = () => {
         <FormLabel>Email</FormLabel>
         <Input
           placeholder='Enter your email'
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormControl>
@@ -36,6 +96,7 @@ const Login = () => {
           <Input
             type={show ? "text" : "password"}
             placeholder='Enter your password'
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width={"4.5rem"}>
@@ -50,7 +111,8 @@ const Login = () => {
         colorScheme={"blue"}
         width='100%'
         style={{ marginTop: 15 }}
-        onClick={submitHandler}>
+        onClick={submitHandler}
+        isLoading={loading}>
         Login
       </Button>
 
