347b38b5e02e70daf40b2ed6652bf3ad94c9dec2
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-07-05 23:18:21 +0500

    Chat UI

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const ChatBox = () => {
+
+
   return <div>ChatBox</div>;
 };
 
--- frontend/src/components/misc/SideDrawer.js
+++ frontend/src/components/misc/SideDrawer.js
@@ -1,12 +1,119 @@
-import { BellIcon } from "@chakra-ui/icons";
-import { Box, Button, Tooltip, Text, Menu, MenuButton } from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Tooltip,
+  Text,
+  Menu,
+  MenuButton,
+  Avatar,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  DrawerFooter,
+  useToast,
+} from "@chakra-ui/react";
+
+import { ChatState } from "../../Context/ChatProvider";
 import React, { useState } from "react";
+import ProfileModal from "./ProfileModal";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import ChatLoading from "../ChatLoading";
+import UserListItem from "../UserAvatar/UserListItem";
 
 const SideDrawer = () => {
-  const [search, setSearch] = useState("second");
+  const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
+
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
+
+  const toast = useToast();
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to load the search results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+  const accessChat = async (userId) => {
+    try {
+      setLoadingChat(true);
+
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.post("/api/chat", { userId }, config);
+
+      
+
+      setSelectedChat(data);
+      setLoadingChat(false);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
   return (
     <>
@@ -19,7 +126,7 @@ const SideDrawer = () => {
         p='5px 10px 5px 10px'
         borderWidth='5px'>
         <Tooltip label='Search users to chat' hasArrow placement='bottom-end'>
-          <Button variant='ghost'>
+          <Button variant='ghost' onClick={onOpen}>
             <i className='fas fa-search'></i>
             <Text d={{ base: "none", md: "flex" }} px='4'>
               Search User
@@ -31,13 +138,75 @@ const SideDrawer = () => {
           Talk-A-Tive
         </Text>
 
-        <Menu>
-          <MenuButton p='1'>
-            <BellIcon fontSize='2xl' m='1' />
-          </MenuButton>
-          {/* <MenuList></MenuList> */}
-        </Menu>
+        <div>
+          <Menu>
+            <MenuButton p='1'>
+              <BellIcon fontSize='2xl' m='1' />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size='sm'
+                cursor='pointer'
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </Box>
+
+      <Drawer
+        placement='left'
+        isOpen={isOpen}
+        onClose={onClose}
+        finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Search Users</DrawerHeader>
+
+          <DrawerBody>
+            <Box display='flex' pb='2'>
+              <Input
+                placeholder='Search by name or email'
+                mr='2'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult?.map((user) => {
+                return (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => accessChat(user._id)}></UserListItem>
+                );
+              })
+            )}
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
